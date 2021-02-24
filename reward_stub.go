de8b99e9bb2e4891c4c01878ae70d3e6d5580b47
Brian Amadio <brian.amadio@stitchfix.com>
2021-02-24 13:00:12 -0800

    contextual reward stub

--- reward_stub.go
+++ reward_stub.go
@@ -1,11 +1,33 @@
 package mab
 
-import "context"
+import (
+	"context"
+	"fmt"
+)
 
 type RewardStub struct {
 	Rewards []Dist
 }
 
-func (s *RewardStub) GetRewards(context.Context) ([]Dist, error) {
+func (s *RewardStub) GetRewards(context.Context, interface{}) ([]Dist, error) {
 	return s.Rewards, nil
+}
+
+type ContextualRewardStub struct {
+	Rewards map[string][]Dist
+}
+
+func (c *ContextualRewardStub) GetRewards(ctx context.Context, banditContext interface{}) ([]Dist, error) {
+	key, ok := banditContext.(string)
+	if !ok {
+		return nil, fmt.Errorf("banditContext must be a string")
+	}
+
+	val, ok := c.Rewards[key]
+
+	if !ok {
+		return nil, fmt.Errorf("no distributions for %s", val)
+	}
+
+	return val, nil
 }
